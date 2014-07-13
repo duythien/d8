8b7989c9cbfb19573c478e3b1cfb91c17f71996f
duythien <fcduythien@gmail.com>
2014-07-13 10:34:41 +0700

    add example form

---
+++ day1/hello_block/src/Form/HelloGetFormPage.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\hello_block\Form\HelloGetFormPage.
+ */
+
+namespace Drupal\hello_block\Form;
+
+use Drupal\Core\Form\FormInterface;
+
+/**
+ * File test form class.
+ */
+class HelloGetFormPage implements FormInterface {
+
+  /**
+   * Returns a unique string identifying the form.
+   *
+   * @return string
+   *   The unique string identifying the form.
+   */
+  public function getFormID() {
+    return 'hello_block';
+  }
+
+  /**
+   * Form constructor.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param array $form_state
+   *   An associative array containing the current state of the form.
+   *
+   * @return array
+   *   The form structure.
+   */
+  public function buildForm(array $form, array &$form_state) {
+    $form['intro'] = array(
+      '#markup' => t('Use this form to send a message to an e-mail address. No spamming!'),
+    );
+    $form['email'] = array(
+      '#type' => 'textfield',
+      '#title' => t('E-mail address'),
+      '#required' => TRUE,
+    );
+    $form['message'] = array(
+      '#type' => 'textarea',
+      '#title' => t('Message'),
+      '#required' => TRUE,
+    );
+    $form['submit'] = array(
+      '#type' => 'submit',
+      '#value' => t('Submit'),
+    );
+    return $form;
+  }
+
+  /**
+   * Form validation handler.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param array $form_state
+   *   An associative array containing the current state of the form.
+   */
+  public function validateForm(array &$form, array &$form_state) {
+    if (!valid_email_address($form_state['values']['email'])) {
+      form_set_error('email', t('That e-mail address is not valid.'));
+    }
+  }
+
+  /**
+   * Form submission handler.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param array $form_state
+   *   An associative array containing the current state of the form.
+   */
+  public function submitForm(array &$form, array &$form_state) {
+    hello_block_mail_send($form_state['values']);
+  }
+}
